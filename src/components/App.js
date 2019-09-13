f849b0dd3ff8c4ae0f45caaf1a74f6b0e3079521
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-13 10:50:01 -0700

    rendering FileUpload component to test it

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import FileUpload from './file-upload/FileUpload';
 
 export default function App() {
-  return<h1>Hello World</h1>;
+  return(
+    <FileUpload />
+  );
 }
