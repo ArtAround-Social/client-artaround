5f9d18a37b5083845211e0ac4ed20fa683aaa835
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-12 21:40:15 -0700

    Upload component

---
+++ src/components/file-upload/Upload.js
@@ -0,0 +1,29 @@
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
+import { useDropzone } from 'react-dropzone';
+
+function FileUpload(){
+  const onDrop = useCallback(
+    ([file]) => {
+      console.log(file);
+    }
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return(
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive 
+        ? (<p>Drop the image here ...</p>)
+        : (<p>Drag and drop the image here, or click to select image</p>)
+      }
+    </div>
+  );
+}
+
+FileUpload.propTypes = {
+  onDrop: PropTypes.func.isRequired,
+};
+
+export default FileUpload;
