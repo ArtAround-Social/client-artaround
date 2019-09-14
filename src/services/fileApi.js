37849f63bc1048aab06264e99b17e75df5a062ac
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-13 17:12:42 -0700

    fileApi

--- src/services/fileApi.js
+++ src/services/fileApi.js
@@ -1,10 +1,12 @@
-export const postImage = (image) => {
-  return fetch(`${process.env.API_URL}/api/v1/files`, {
+// const artist = '5d7bfa5d0ae46c485efe113d';
+
+export const postImage = (file) => {
+  const data = new FormData();
+  data.append('file', file);
+  // data.append('artistId', artist);
+  return fetch(`${process.env.API_URL}/api/v1/files/artwork`, {
     method: 'POST',
-    body: JSON.stringify({ image }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
+    body: data
   })
     .then(res => {
       if(!res.ok) throw 'Could not upload the image';
