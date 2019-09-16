8ec45ef3995411450d60aa3561e972c1c53db74a
Vasily93 <markovavasily@gmail.com>
2019-09-16 09:01:31 -0700

    deleted console log

--- src/services/allImagesCall.js
+++ src/services/allImagesCall.js
@@ -2,6 +2,8 @@ export const getAllImages = () => {
   return fetch('https://picsum.photos/v2/list')
     .then(res => {
       if(!res.ok) throw 'can not get all images';
+      console.log(res);
+      
       return res.json();
       
     });
