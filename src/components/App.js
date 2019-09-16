a5b138a07cdfa3fff704d2b50dab5f05212e5c0d
Vasily93 <markovavasily@gmail.com>
2019-09-16 14:15:34 -0700

    added header container

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Routes from './landing_page/Routes';
+import HeaderContainer from '../containers/HeaderContainer';
 
 export default function App() {
   return(
   <>
+    <HeaderContainer/>
     <Routes/>
   </>
   );
