a1932ae4011006b5684c357032e33fbf71a40da2
Vasily93 <markovavasily@gmail.com>
2019-09-16 14:11:22 -0700

    refactored header in one component and connected to state

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import AccountType from './AccountType';
-import UserProfile from './UserProfile';
-import Logout from './Logout';
+import PropTypes from 'prop-types';
 import styles from './Header.css';
+import userImage from './user2.jpg';
 
-export default function Header() {
+export default function Header({ user_type, handleLogout, name }) {
   return(
     <section className={styles.section}>
-      <div className={styles.headerleft}>
-        <AccountType />
-      </div>
-      <div className={styles.headerright}>
-        <UserProfile />
-        <Logout />
-      </div>
+      <h3>{user_type} Account</h3>
+      <button className={styles.button} onClick={handleLogout}>Logout</button>
+      {name}
+      <img src={userImage} alt='user png' height='40'/>
     </section>
   );
 }
+
+Header.propTypes = {
+  user_type: PropTypes.string,
+  handleLogout: PropTypes.func,
+  name: PropTypes.string
+};
