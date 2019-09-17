ef813fce68b10ef3a652d02fd250bbf928cafc7a
Vasily93 <markovavasily@gmail.com>
2019-09-17 15:37:33 -0700

    changed buttons to links in partner item

--- src/components/partners/PartnerItem.js
+++ src/components/partners/PartnerItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-function PartnerItem({ partner, goToPofile }) {
+function PartnerItem({ partner }) {
   return(
     <div>
       {partner.name}
-      <button onClick={goToPofile}>Profile</button>
+      <Link to='artist/:id'>Go to Profile</Link> 
     </div>
   );
 }
@@ -13,8 +14,8 @@ function PartnerItem({ partner, goToPofile }) {
 PartnerItem.propTypes = {
   partner: PropTypes.shape({
     name: PropTypes.string.isRequired
-  }),
-  goToPofile: PropTypes.func
+  })
 };
 
 export default PartnerItem;
+//also Link suposed to go to galleries too
