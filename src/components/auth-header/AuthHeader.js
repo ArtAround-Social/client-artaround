1eb72decf10d252cb6e6c33e98bcd59e070afe47
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-16 20:03:41 -0700

    AuthHeader component and added to routes for testing

--- src/components/auth-header/AuthHeader.js
+++ src/components/auth-header/AuthHeader.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { useAuth0 } from '../../../authProvider';
 
 export default function AuthHeader() {
-  const { isAuthenticated, auth0Client } = useAuth0();
+  const { isAuthenticated, auth0Client, user } = useAuth0();
   const login = () => auth0Client.loginWithRedirect();
   const logout = () => auth0Client.logout({ returnTo: 'http://localhost:7890' });
+
+  console.log(user && 'unknown user');
 
   return(
     <>
--- src/components/landing_page/Routes.js
+++ src/components/landing_page/Routes.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter as Switch, Route } from 'react-router-dom';
 import ArtistFormContainer from '../../containers/GalleryFormContainer';
 import GalleryFormContainer from '../../containers/GalleryFormContainer';
-import LandingPageContainer from '../../containers/LandingPageContainer';
+// import LandingPageContainer from '../../containers/LandingPageContainer';
+import AuthHeader from '../auth-header/AuthHeader';
 
 export default function Routes() {
   return(
     <Switch>
       <Route path='/artistform' component={ArtistFormContainer}/>
       <Route path='/galleryform' component={GalleryFormContainer}/>
-      <Route exact path='/' component={LandingPageContainer}/>
+      <Route exact path='/' component={AuthHeader}/>
     </Switch>
   );
 }
+
+{/* <Route exact path='/' component={LandingPageContainer}/> */}
