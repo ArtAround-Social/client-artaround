465fd90b1697074278087ed18b8e455557b9ce77
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-17 20:28:27 -0700

    UserApi creates user correctly

--- src/services/userApi.js
+++ src/services/userApi.js
@@ -1,13 +1,37 @@
 const apiUrl = process.env.API_URL;
 // const herokuURL = 'https://artaround-test-app.herokuapp.com/api/v1/users';
 
-export const createUser = ({ user_type, name, location, userAuth0Id, phone, email, poster, styles, mediums, rules }) => {
+export const createUser = ({
+  userType,
+  name,
+  galleryName,
+  location,
+  styles,
+  mediums,
+  poster,
+  userAuth0Id,
+  phone,
+  email,
+  rules
+}) => {
   return fetch(`${apiUrl}/api/v1/users`, {
     method: 'POST',
     headers: {
-      'Content-type': 'application/json'
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ user_type, name, location, userAuth0Id, phone, email, poster, styles, mediums, rules })
+    body: JSON.stringify({
+      userType,
+      name,
+      galleryName,
+      location,
+      styles,
+      mediums,
+      poster,
+      userAuth0Id,
+      phone,
+      email,
+      rules
+    })
   })
     .then(res => {
       if(!res.ok) throw 'Cannot add user';
@@ -16,12 +40,11 @@ export const createUser = ({ user_type, name, location, userAuth0Id, phone, emai
 };
 
 export const findUser = ({ userAuth0Id }) => {
-  return fetch(`${apiUrl}/api/v1/users/${userAuth0Id}`, {
+  return fetch(`${apiUrl}/api/v1/users/auth0/${userAuth0Id}`, {
     method: 'GET',
     headers: {
       'Content-type': 'application/json'
-    },
-    body: JSON.stringify({ userAuth0Id })
+    }
   })
     .then(res => {
       if(!res.ok) throw 'Cannot find user';
