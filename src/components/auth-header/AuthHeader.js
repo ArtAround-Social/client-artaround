58d2a6aaf30fa26a6a15dc8003689af0daf784ae
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-17 20:33:55 -0700

    AuthHeader component in progress

--- src/components/auth-header/AuthHeader.js
+++ src/components/auth-header/AuthHeader.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useAuth0 } from '../../AuthProvider';
 import Header from '../header/Header';
+// import { findUser } from '../../services/userApi';
+import UserForm from '../signup_forms/UserForm';
 
 export default function AuthHeader() {
   const { isAuthenticated, auth0Client, user } = useAuth0();
@@ -8,17 +10,24 @@ export default function AuthHeader() {
   const logout = () => auth0Client.logout({ returnTo: 'http://localhost:7890' });
 
   // console.log('### AUTH ###', user ? Object.keys(user) : 'No user');
+  // if(isAuthenticated && !findUser(user.sub)) userComponent = <UserForm />;
+
   user
     ? console.log(Object.keys(user), user.sub)
     : 'No user';
   const userType = 'artist';
   const name = 'George';
 
-  // console.log(user && 'unknown user');
+  // if(user) console.log(findUser(user.sub));
+  
 
   return(
     <>
       {!isAuthenticated && <button onClick={login}>Login</button>}
+      {/* {isAuthenticated && !findUser('user.sub') &&
+        <UserForm />
+      } */}
+      <UserForm />
       {isAuthenticated &&
         <>
           <Header user_type={userType} name={name} />
