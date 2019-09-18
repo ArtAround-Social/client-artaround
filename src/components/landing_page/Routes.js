36e649c35115d90ee9f49822fe1b9269e1165cdd
Vasily93 <markovavasily@gmail.com>
2019-09-18 14:06:38 -0700

    added route for userhome

--- src/components/landing_page/Routes.js
+++ src/components/landing_page/Routes.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Switch, Route } from 'react-router-dom';
-import ArtistHome from '../home_pages/ArtistHome';
-import GalleryHome from '../home_pages/GalleryHome';
 import AllArtistsContainer from '../../containers/AllArtistsContainer';
+import UserHome from '../home_pages/UserHome';
 import ArtistProfile from '../profiles/ArtistProfile';
 import GalleryProfile from '../profiles/GalleryProfile';
 import LandingPage from './LandingPage';
@@ -10,11 +9,10 @@ import LandingPage from './LandingPage';
 export default function Routes() {
   return(
     <Switch>
-      <Route path='/artisthome' component={ArtistHome}/>
+      <Route path='/userhome' component={UserHome}/>
       <Route path='/artist/:id' component={ArtistProfile}/>
       <Route path='/gallery/:id' component={GalleryProfile}/>
       <Route path='/allartists' component={AllArtistsContainer}/>
-      <Route path='/galleryhome' component={GalleryHome}/>
       <Route exact path='/' component={LandingPage}/>
     </Switch>
   );
