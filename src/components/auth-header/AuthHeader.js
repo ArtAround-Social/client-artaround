099ac9cad67524f75b08355fb9cf50143ec2c03b
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-17 17:31:47 -0700

    AuthHeader component

--- src/components/auth-header/AuthHeader.js
+++ src/components/auth-header/AuthHeader.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { useAuth0 } from '../../../authProvider';
+import { useAuth0 } from '../../AuthProvider';
+import Header from '../header/Header';
 
 export default function AuthHeader() {
   const { isAuthenticated, auth0Client, user } = useAuth0();
   const login = () => auth0Client.loginWithRedirect();
   const logout = () => auth0Client.logout({ returnTo: 'http://localhost:7890' });
 
-  console.log(user && 'unknown user');
+  // console.log('### AUTH ###', user ? Object.keys(user) : 'No user');
+  user
+    ? console.log(Object.keys(user), user.sub)
+    : 'No user';
+  const userType = 'artist';
+  const name = 'George';
+
+  // console.log(user && 'unknown user');
 
   return(
     <>
       {!isAuthenticated && <button onClick={login}>Login</button>}
-      {isAuthenticated && <button onClick={logout}>Logout</button>}
+      {isAuthenticated &&
+        <>
+          <Header user_type={userType} name={name} />
+          <button onClick={logout}>Logout</button>
+        </>
+      }
     </>
   );
 }
