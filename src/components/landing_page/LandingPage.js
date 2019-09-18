197ca1c2263d537ed6442d2653cb63dfde0a710c
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-18 12:04:56 -0700

    Fixed some messed up routes stuff

--- src/components/landing_page/LandingPage.js
+++ src/components/landing_page/LandingPage.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import styles from './LandingPage.css';
 function LandingPage({ handleSignin, email, password }) {
   return(
     <div className={styles.body}>
       <h3 className={styles.header1}>CREATE ACCOUNT</h3>
-      <p className={styles.signup}>
-        <Link className={styles.artist} to='/artistform'>Artist Account</Link>
-        <Link className={styles.gallery} to='/galleryform'> Gallery Account</Link>
-      </p>
-      
+      <p className={styles.signup}></p>
       <h3 className={styles.header2}>ALREADY HAVE AN ACOUNT</h3>
       <form onSubmit={handleSignin}>
         <input value={email} placeholder='email'/><br/>
--- src/components/landing_page/Routes.js
+++ src/components/landing_page/Routes.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Switch, Route } from 'react-router-dom';
-import ArtistFormContainer from '../../containers/ArtistFormContainer';
-import GalleryFormContainer from '../../containers/GalleryFormContainer';
 import LandingPageContainer from '../../containers/LandingPageContainer';
 import ArtistHome from '../home_pages/ArtistHome';
 import GalleryHome from '../home_pages/GalleryHome';
@@ -17,8 +15,6 @@ export default function Routes() {
       <Route path='/gallery/:id' component={GalleryProfile}/>
       <Route path='/allartists' component={AllArtistsContainer}/>
       <Route path='/galleryhome' component={GalleryHome}/>
-      <Route path='/artistform' component={ArtistFormContainer}/>
-      <Route path='/galleryform' component={GalleryFormContainer}/>
       <Route exact path='/' component={LandingPageContainer}/>
     </Switch>
   );
