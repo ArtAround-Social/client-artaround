1abd92208a77ba8085eb693fda530cd2fb69dfbf
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-18 15:00:01 -0700

    getting user in UserHome

--- src/components/home_pages/UserHome.js
+++ src/components/home_pages/UserHome.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import AuthHeader from '../auth-header/AuthHeader';
+import { useAuth0 } from '../../AuthProvider';
 // import HeaderContainer from '../../containers/HeaderContainer';
 // import UserImagesContainer from '../../containers/UserImagesContainer';
 // import UserPartnersContainer from '../../containers/UserPartnersContainer';
 
 export default function UserHome() {
-  return(
-    <>
-      <AuthHeader />
-      <h2>userHome</h2>
-        {/* <HeaderContainer/> */}
-        {/* <UserImagesContainer/> */}
-        {/* <UserPartnersContainer/> */}
-    </>
-  );
+  const { user } = useAuth0();
+  
+  if(user){
+    console.log('##### USER #####', user.sub);
+    return(
+      <>
+        <AuthHeader />
+        <h2>userHome</h2>
+          {/* <HeaderContainer/> */}
+          {/* <UserImagesContainer/> */}
+          {/* <UserPartnersContainer/> */}
+      </>
+    );
+  } else {
+    return<h1>Loading</h1>;
+  }
+  
 }
