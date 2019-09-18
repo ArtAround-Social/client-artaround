682efa6805a639f2e24d901507639af99a022634
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-17 17:30:58 -0700

    userApi createUser, and findUser

--- src/services/userApi.js
+++ src/services/userApi.js
@@ -1,27 +1,30 @@
-export const addGallery = ({ galleryName, galleryLoaction, owener, email, password }) => {
-  return fetch('https://artaround-test-app.herokuapp.com/api/v1/galleries', {
+const apiUrl = process.env.API_URL;
+// const herokuURL = 'https://artaround-test-app.herokuapp.com/api/v1/users';
+
+export const createUser = ({ user_type, name, location, userAuth0Id, phone, email, poster, styles, mediums, rules }) => {
+  return fetch(`${apiUrl}/api/v1/users`, {
     method: 'POST',
     headers: {
       'Content-type': 'application/json'
     },
-    body: JSON.stringify({ galleryName, galleryLoaction, owener, email, password })
+    body: JSON.stringify({ user_type, name, location, userAuth0Id, phone, email, poster, styles, mediums, rules })
   })
     .then(res => {
-      if(!res.ok) throw 'can not add gallery';
+      if(!res.ok) throw 'Cannot add user';
       return res.json();
     });
 };
 
-export const addArtist = ({ artistName, artistLocation, mediums, styles, email, password }) => {
-  return fetch('https://artaround-test-app.herokuapp.com/api/v1/artists', {
-    method: 'POST',
+export const findUser = ({ userAuth0Id }) => {
+  return fetch(`${apiUrl}/api/v1/users/${userAuth0Id}`, {
+    method: 'GET',
     headers: {
       'Content-type': 'application/json'
     },
-    body: JSON.stringify({ artistName, artistLocation, mediums, styles, email, password })
+    body: JSON.stringify({ userAuth0Id })
   })
     .then(res => {
-      if(!res.ok) throw 'can not create artist';
+      if(!res.ok) throw 'Cannot find user';
       return res.json();
     });
 };
