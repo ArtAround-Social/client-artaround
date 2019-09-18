9629ab96d2b25cc47cbd565e17e60efd60490f67
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-18 12:04:03 -0700

    Loading up userForm in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Routes from './landing_page/Routes';
+import FileUpload from './file-upload/FileUpload';
+import UserForm from './auth-header/UserForm';
 
 export default function App() {
   return(
   <>
+    <UserForm />
     <Routes/>
+    <FileUpload />
   </>
   );
 }
