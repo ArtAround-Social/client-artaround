7ea81bf46a48065c99eac108087d5e8866fc387c
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-17 17:33:52 -0700

    Loading AuthHeader in app for testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Routes from './landing_page/Routes';
-import HeaderContainer from '../containers/HeaderContainer';
+// import Routes from './landing_page/Routes';
+// import HeaderContainer from '../containers/HeaderContainer';
 import FileUpload from './file-upload/FileUpload';
+import AuthHeader from './auth-header/AuthHeader';
 
 export default function App() {
   return(
   <>
-    <HeaderContainer/>
-    <Routes/>
+    {/* <HeaderContainer/> */}
+    <AuthHeader/>
     <FileUpload />
   </>
   );
