0fbe73abbae73b50f4701e5fd5e38efacdc73ce6
Vasily93 <markovavasily@gmail.com>
2019-09-17 18:50:05 -0700

    added file upload to home pages. deleted from app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Routes from './landing_page/Routes';
-import FileUpload from './file-upload/FileUpload';
 
 export default function App() {
   return(
   <>
     <Routes/>
-    <FileUpload />
   </>
   );
 }
--- src/components/home_pages/GalleryHome.js
+++ src/components/home_pages/GalleryHome.js
@@ -3,6 +3,7 @@ import HeaderContainer from '../../containers/HeaderContainer';
 import UserImagesContainer from '../../containers/UserImagesContainer';
 import UserPartnersContainer from '../../containers/UserPartnersContainer';
 import { Link } from 'react-router-dom';
+import FileUpload from '../file-upload/FileUpload';
 
 
 export default function ArtistHome() {
@@ -10,6 +11,7 @@ export default function ArtistHome() {
     <>
       <HeaderContainer/>
       <UserImagesContainer/>
+      <FileUpload/>
       <UserPartnersContainer/>
       <Link to='/allartists'>Find New Artists</Link>    
     </>
