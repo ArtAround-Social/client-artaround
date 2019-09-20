36a6be5f555090c6a6bfa75846076f225d74c06a
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-19 23:13:35 -0700

    Artist Profile changed to class component Mistake?

--- src/components/profiles/ArtistProfile.js
+++ src/components/profiles/ArtistProfile.js
@@ -1,33 +1,43 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ProfileImagesContainer from '../../containers/ProfileImagesContainer';
 import ProfilePartnersContainer from '../../containers/ProfilePartnersContainer';
 import ArtistInfoContainers from '../../containers/ArtistInfoContainers';
 import RequestButton from './RequestButton';
 import AuthHeader from '../auth-header/AuthHeader';
 import styles from './Profile.css';
+import { addUserById } from '../../actions/userActions';
 
-function ArtistProfile() {
-  return(
-    <div>
-      <AuthHeader/>
-      <h2>artist</h2>
-      <div className={styles.images}>
-        <ProfileImagesContainer/>
+class ArtistProfile extends Component {
+  static propTypes = {
+    match: PropTypes.object.isRequired,
+    location: PropTypes.object,
+    history: PropTypes.object
+  }
+
+  componentDidMount(){
+    console.log('PARAMS', this.props.match.params.id);
+    addUserById(this.props.match.params.id);
+  }
+
+  render() {
+    return(
+      <div>
+        <AuthHeader/>
+        <h2>artist</h2>
+        <div className={styles.images}>
+          <ProfileImagesContainer/>
+        </div>
+        <div className={styles.info}>
+          <ArtistInfoContainers/>
+        </div>
+        <div className={styles.partners}>
+          <ProfilePartnersContainer/>
+          <RequestButton/>
+        </div>
       </div>
-      <div className={styles.info}>
-        <ArtistInfoContainers/>
-      </div>
-      <div className={styles.partners}>
-        <ProfilePartnersContainer/>
-        <RequestButton/>
-      </div>
-    </div>
-  );
+    );
+  }
 }
-
-ArtistProfile.propTypes = {
-
-};
 
 export default ArtistProfile;
