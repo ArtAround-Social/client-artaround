dc70be1c007a4a061c5967de144d9c70cb549036
Vasily93 <markovavasily@gmail.com>
2019-09-20 08:30:37 -0700

    changed placeholder

--- src/components/all_artists/AnArtist.js
+++ src/components/all_artists/AnArtist.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './AllArtists.css';
+import placeholder from '../../../assets/logo.png';
 
 function AnArtist({ artist, imgUrl }) {
-  const placeholder = '../../../assets/placeholder.jpg';
   return(
     <Link className={styles.artist} to={(`/artist/${artist._id}`)}>
-      <p>{artist.name}</p>
       <img  src={imgUrl || placeholder} width='150px'/>
+      <p>{artist.name}</p>
     </Link>
   );
 }
