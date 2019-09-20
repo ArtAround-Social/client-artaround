d5fbc8f1af69eb4b4edcdd4623fab7f8a5365540
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-19 23:12:20 -0700

    userActions updated

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,9 +1,18 @@
 // import { userById } from '../services/userApi';
-import { findUser } from '../services/userApi';
+import { findUserByAuthId, userById } from '../services/userApi';
 
 
-export const GET_USER = 'GET_USER';
-export const getUser = (userSub) => ({
-  type: GET_USER,
-  payload: findUser({ userAuth0Id: userSub })
+export const GET_AUTH_USER = 'GET_AUTH_USER';
+export const getAuthUser = (userSub) => ({
+  type: GET_AUTH_USER,
+  payload: findUserByAuthId({ userAuth0Id: userSub })
 });
+
+export const ADD_USER_BY_ID = 'ADD_USER_BY_ID';
+export const addUserById = (userId) => {
+  userById({ userId }).then(res => console.log('ACTION ####', typeof res));
+  return({
+    type: ADD_USER_BY_ID,
+    payload: userById({ userId })
+  });
+};
