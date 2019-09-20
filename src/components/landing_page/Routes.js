51785d7380a9b9b895c315b0d46a7dfad265f850
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-20 01:28:17 -0700

    artist/:id route takes you to ArtistInfoContainer

--- src/components/landing_page/Routes.js
+++ src/components/landing_page/Routes.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter as Switch, Route } from 'react-router-dom';
 import AllArtistsContainer from '../../containers/AllArtistsContainer';
 import UserHome from '../home_pages/UsrHome';
-import ArtistProfile from '../profiles/ArtistProfile';
+// import ArtistProfile from '../profiles/ArtistProfile';
 import GalleryProfile from '../profiles/GalleryProfile';
 import LandingPage from './LandingPage';
 import { withSession } from '../../AuthProvider';
 import AuthContainer from '../../containers/AuthContainer';
+import ArtistInfoContainer from '../../containers/ArtistInfoContainer';
 
 
 
@@ -15,7 +16,7 @@ export default function Routes() {
     <Switch>
       <Route path='/auth' component={withSession(AuthContainer)}/>
       <Route path='/userhome' component={withSession(UserHome)}/>
-      <Route path='/artist/:id' component={ArtistProfile}/>
+      <Route path='/artist/:id' component={ArtistInfoContainer}/>
       <Route path='/gallery/:id' component={GalleryProfile}/>
       <Route path='/allartists' component={AllArtistsContainer}/>
       <Route exact path='/' component={LandingPage}/>
