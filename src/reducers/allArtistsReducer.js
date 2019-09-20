4f9658f7b9150f03b6fbf4387a8a5c5c1f9f7efa
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-19 19:51:05 -0700

    removed static data

--- src/reducers/allArtistsReducer.js
+++ src/reducers/allArtistsReducer.js
@@ -1,9 +1,7 @@
 import { GET_ARTISTS } from '../actions/allArtistsActions';
 
 const initialState = {
-  allartists: [
-    { id: 1253, name: 'pavel', imgUrl: 'https://www.goodfreephotos.com/albums/people/man-sitting-on-the-beach-monochrome.jpg' },
-  ]
+  allartists: []
 };
 
 export default function reducer(state = initialState, action) {
