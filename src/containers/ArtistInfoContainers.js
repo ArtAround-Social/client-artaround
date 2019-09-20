2f129da5b8e294b8be374adcf198a498eb9d3535
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-19 23:14:40 -0700

    Updated ArtistInfoContainer with the correct info

--- src/containers/ArtistInfoContainers.js
+++ src/containers/ArtistInfoContainers.js
@@ -1,15 +1,9 @@
 import { connect } from 'react-redux';
 import ArtistProfileInfo from '../components/profiles/ArtistProfileInfo';
-import { getArtistName, getArtistLocation, getArtistStyles, getArtistMediums, getArtistPoster, getArtistPhone, getArtistEmail } from '../selectors/allSelectors';
+import { getArtistInfoUser } from '../selectors/allSelectors';
 
 const mapStateToProps = state => ({
-  artistName: getArtistName(state),
-  artistLocation: getArtistLocation(state),
-  styles: getArtistStyles(state),
-  mediums: getArtistMediums(state),
-  poster: getArtistPoster(state),
-  phone: getArtistPhone(state),
-  email: getArtistEmail(state)
+  user: getArtistInfoUser(state)
 });
 
 export default connect(
